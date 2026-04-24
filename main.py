a2d4d95ddf6e9fda71d67c2c6361dab2c887f84f
imZontik <elvin-nadirov-321@mail.ru>
2026-04-24 11:39:36 +0300

    last

--- main.py
+++ main.py
@@ -84,10 +84,13 @@ async def create_tables():
             ("家", "jiā", "Дом", "цзя"),
             ("书", "shū", "Книга", "шу"),
         ]
-        # Чистим таблицу, чтобы гарантированно убрать старые английские переводы
-        await db.execute("DELETE FROM words")
         await db.executemany(
-            'INSERT INTO words (chinese, pinyin, translation, russian_transcription) VALUES (?, ?, ?, ?)',
+            '''INSERT INTO words (chinese, pinyin, translation, russian_transcription)
+               VALUES (?, ?, ?, ?)
+               ON CONFLICT(chinese) DO UPDATE SET
+                   pinyin=excluded.pinyin,
+                   translation=excluded.translation,
+                   russian_transcription=excluded.russian_transcription''',
             initial_words
         )
 
@@ -104,11 +107,21 @@ async def create_tables():
             ('Транспорт', '到火车站多少钱？', 'dào huǒchē zhàn duōshǎo qián?', 'Сколько до вокзала?'),
             ('Транспорт', '我要买票', 'wǒ yào mǎi piào', 'Я хочу купить билет'),
         ]
-        await db.execute("DELETE FROM tourist_phrases")
         await db.executemany(
-            'INSERT INTO tourist_phrases (category, chinese, pinyin, translation) VALUES (?, ?, ?, ?)',
+            '''INSERT INTO tourist_phrases (category, chinese, pinyin, translation)
+               VALUES (?, ?, ?, ?)
+               ON CONFLICT(chinese) DO UPDATE SET
+                   category=excluded.category,
+                   pinyin=excluded.pinyin,
+                   translation=excluded.translation''',
             phrases
         )
+
+        # Удалим возможную ошибочную запись из старых версий БД
+        await db.execute("DELETE FROM words WHERE chinese = 'книга'")
+        # Удалим старые записи с английскими переводами (содержат латиницу)
+        await db.execute("DELETE FROM words WHERE translation GLOB '*[A-Za-z]*'")
+        await db.execute("DELETE FROM tourist_phrases WHERE translation GLOB '*[A-Za-z]*'")
         await db.commit()
 
 
